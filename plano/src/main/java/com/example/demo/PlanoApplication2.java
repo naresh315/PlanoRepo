6c41b898413974f9725a6f8744a0fba9f2d5b4cb
HP <HP@DESKTOP-I28OP1B.attlocal.net>
2022-10-01 18:27:42 -0500

    Second commit to planos

---
+++ plano/src/main/java/com/example/demo/PlanoApplication2.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanoApplication2 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanoApplication2.class, args);
+	}
+
+}
