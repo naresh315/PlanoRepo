698a98efa641cea046f7ae0c55556099991993f6
naresh315 <naresh07315@gmail.com>
2022-10-28 07:32:56 -0500

    Update ArraysPrograms.java

--- ArraysPrograms.java
+++ ArraysPrograms.java
@@ -1,5 +1,6 @@
-<br><br>Peak Element<br><br>
-int findPeakELement(int arr[], arrlength){
+Peak Element  
+Solution 1.
+  int findPeakELement(int arr[], arrlength){
     if (arrlength == 1)
       return arr[0];//return the element if there's only one element in the array
     if (arr[0] >= arr[1])
@@ -18,43 +19,43 @@ int findPeakELement(int arr[], arrlength){
     return arr[0];
 }
 
-Find the minimum and maximum element in an array<br><br>
-Write a program to reverse the array<br><br>
-Write a program to sort the given array<br><br>
-Find the Kth largest and Kth smallest number in an array<br><br>
-Find the occurrence of an integer in the array<br><br>Sort the array of 0s, 1s, and 2s<br><br>
-Subarray with given Sum<br><br>
-Move all the negative elements to one side of the array<br><br>
-Find the Union and Intersection of the two sorted arrays<br><br>Write a program to cyclically rotate an array by one<br><br>Find the missing integer<br><br>
-Count Pairs with given sum<br><br>
-Find duplicates in an array<br><br>
-Sort an Array using the Quicksort algorithm<br><br>
-Find common elements in three sorted arrays<br><br>
-Find the first repeating element in an array of integers<br><br>
-Find the first non-repeating element in a given array of integers<br><br>
-Subarrays with equal 1s and 0s<br><br>
-Rearrange the array in alternating positive and negative items<br><br>
-Find if there is any subarray with a sum equal to zero<br><br>
-Find the Largest sum contiguous Subarray<br><br>
-Find the factorial of a large number<br><br>
-Find Maximum Product Subarray<br><br>
-Find the longest consecutive subsequence<br><br>
+Find the minimum and maximum element in an array
+Write a program to reverse the array
+Write a program to sort the given array
+Find the Kth largest and Kth smallest number in an array
+Find the occurrence of an integer in the arraySort the array of 0s, 1s, and 2s
+Subarray with given Sum
+Move all the negative elements to one side of the array
+Find the Union and Intersection of the two sorted arraysWrite a program to cyclically rotate an array by oneFind the missing integer
+Count Pairs with given sum
+Find duplicates in an array
+Sort an Array using the Quicksort algorithm
+Find common elements in three sorted arrays
+Find the first repeating element in an array of integers
+Find the first non-repeating element in a given array of integers
+Subarrays with equal 1s and 0s
+Rearrange the array in alternating positive and negative items
+Find if there is any subarray with a sum equal to zero
+Find the Largest sum contiguous Subarray
+Find the factorial of a large number
+Find Maximum Product Subarray
+Find the longest consecutive subsequence
 Find the minimum element in a rotated and sorted array
-<br><br>Max sum in the configuration<br><br>
-Minimum Platforms<br><br>Minimize the maximum difference between the heights<br><br>
-Minimum number of jumps to reach the end<br><br>
-Stock Span problem<br><br>Find a triplet that sums to a given value<br><br>
-Smallest positive missing number<br><br>Find the row with a maximum number of 1’s<br><br>
-Print the matrix in a Spiral manner<br><br>
-Find whether an array is a subset of another array<br><br>
-Implement two Stacks in an array<br><br>Majority Element<br><br>
-Wave Array<br><br>Trapping Rainwater<br><br>Level 3<br><br>
-Maximum Index<br><br>Max sum path in two arrays<br><br>
-Find Missing And Repeating<br><br>Stock buy and sell Problem<br><br>
-Pair with the given sum in a sorted array<br><br>
-Chocolate Distribution Problem<br><br>Partition Equal Subset Sum<br>
-Smallest Positive integer that can’t be represented as a sum<br><br>
+Max sum in the configuration
+Minimum PlatformsMinimize the maximum difference between the heights
+Minimum number of jumps to reach the end
+Stock Span problemFind a triplet that sums to a given value
+Smallest positive missing numberFind the row with a maximum number of 1’s
+Print the matrix in a Spiral manner
+Find whether an array is a subset of another array
+Implement two Stacks in an arrayMajority Element
+Wave ArrayTrapping RainwaterLevel 3
+Maximum IndexMax sum path in two arrays
+Find Missing And RepeatingStock buy and sell Problem
+Pair with the given sum in a sorted array
+Chocolate Distribution ProblemPartition Equal Subset Sum<br>
+Smallest Positive integer that can’t be represented as a sum
 
-Coin Change Problem<br><br>
+Coin Change Problem
 
-Longest Alternating subsequence<br><br> <br><br>
+Longest Alternating subsequence 
